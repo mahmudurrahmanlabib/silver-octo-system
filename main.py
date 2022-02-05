0a72b4d2d91becdb5c11bd92bd01126d64fba964
Mahmudur Rahman Labib <labibrahman555@gmail.com>
2022-02-05 08:07:58 +0600

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import os
+
+def makeCommits (days : int):
+    if days < 1:
+        os.system('git push')
+    else:
+        dates = f"{days} days ago"
+        with open('data.txt', 'a') as file:
+            file.write(f'{dates} <- New Commit!\n')
+        
+        # staging 
+        os.system('git add data.txt')
+
+        # commit 
+        os.system('git commit --date="'+ dates +'" -m "New Commit"')
+
+        return days * makeCommits(days - 1)
+
+makeCommits(767)
